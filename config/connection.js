b81ab52f4b6ca0c3049eacc3be9fdede1fa31afd
Calvin Friedman <oscarsemail19@gmail.com>
2018-07-18 22:48:35 -0400

    heroku

--- config/connection.js
+++ config/connection.js
@@ -5,10 +5,10 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: "k3xio06abqa902qt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-        user: "qfu8fl81ln3zjmo7",
-        password: "ayrcx6tt5twdv3sb",
-        database: "mdwpfj0mk61olm29"
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "dinder_db"
     });
 }
 
