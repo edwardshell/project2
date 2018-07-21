592329bc0e8b68edf7c30ef31bf0e68b6f7f001a
Calvin Friedman <oscarsemail19@gmail.com>
2018-07-21 15:10:04 -0400

    worked on routes

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -1,26 +1,14 @@
 var db = require("../models");
+// var path = require("path");
 
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
+module.exports = function (app) {
+ // Get all examples
+ app.get("/api/restaurants", function (req, res) {
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.json(dbExample);
-    });
-  });
+   db.Restaurant.findAll({})
+     .then(function(response) {
+       res.json(response);
+     });
+ });
+ // });
 };
