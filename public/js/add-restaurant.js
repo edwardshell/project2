93809b04af2b7251c04702c4a884f655847a67bb
Raj Patel <rajpatel@Tonys-MacBook-Pro.local>
2018-07-23 21:50:40 -0400

    added files

--- public/js/add-restaurant.js
+++ public/js/add-restaurant.js
@@ -1,88 +1,87 @@
-var zomato = require("npm-zomato-search");
-
 var globalCity;
 var globalState;
 
-var input = document.getElementById("location");
+
+var input = document.getElementById('location');
 var autocomplete = new google.maps.places.Autocomplete(input, {
-  types: ["(cities)"]
+    types: ['(cities)']
 });
 
-google.maps.event.addListener(autocomplete, "place_changed", function() {
-  var place = autocomplete.getPlace();
-  var city = place.address_components[0].short_name;
-  var state = place.address_components[2].short_name;
-  var lat = place.geometry.location.lat();
-  var lng = place.geometry.location.lng();
-  globalCity = city;
-  globalState = state;
+google.maps.event.addListener(autocomplete, 'place_changed', function () {
+    var place = autocomplete.getPlace();
+    var city = place.address_components[0].short_name;
+    var state = place.address_components[2].short_name;
+    var lat = place.geometry.location.lat();
+    var lng = place.geometry.location.lng();
+    globalCity = city;
+    globalState = state;
 });
 
-$("#submit-pick").click(function() {
-  event.preventDefault();
-  searchZomato();
-});
 
-function searchZomato() {
-  var client = zomato.createClient({
-    userKey: "591abf213dbc64871a5d5ab5740a00ed" //as obtained from [Zomato API](https://developers.zomato.com/apis)
-  });
-
-  var searchTerm = $("#new-restaurant").val().trim();
-  // variable for grabbing search term out of form
-
-  client.search(
-    {
-      q: searchTerm,
-      count: "1"
-      // use restaurant name input to search zomato
-    },
-    function(err, result) {
-      if (!err) {
-        var temp = JSON.parse(result, null, 2);
-        console.log(temp.restaurants[0].restaurant.name);
-        parseZomato(temp);
-        console.log(temp.restaurants[0].restaurant.name);
-      } else {
-        console.log(err);
-      }
+function yelpSearchSettings(restaurant) {
+    return {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=" + globalCity + "," + globalState + "&term=" + restaurant + "&limit=8",
+        "method": "GET",
+        "headers": {
+            "authorization": "Bearer UVD6_jknwmi1GkRxFFkWh7HX-JV_TrlieWHvaSMfi69lmcN6MPUUxk5EGJNnUWmLD3TV5vtrZ25whGiC9gmJRUbhEW5lz3Y6hOXT5oAS-WqK7U5TA772Lt4tBqlaWnYx",
+            "Cache-Control": "no-cache",
+        }
     }
-  );
-}
+};
 
-// Get wanted data from object and assign them to variables and assign values to respective columns in db
 
-function parseZomato(temp) {
-  
-  var name = temp.restaurants[0].restaurant.name;
-  var address = temp.restaurants[0].restaurant.location.address;
-  var cuisines = temp.restaurants[0].restaurant.cuisines;
-  var image = temp.restaurants[0].restaurant.featured_image;
-  var rating = temp.restaurants[0].restaurant.user_rating.aggregate_rating;
-  var website = temp.restaurants[0].restaurant.url;
-  var phone = temp.restaurants[0].restaurant.phone_numbers;
+$("#submit-pick").click(function () {
+    event.preventDefault();
+    searchYelp();
+});
 
-  console.log(data);
-  dinderDB(name, address, cuisines, image, rating, website, phone);
-}
+function searchYelp() {
 
-function dinderDB(name, address, cuisines, image, rating, website, phone) {
-  var restaurant = {
-    user_name: $("#username").val().trim(),
-    group_name: $("#groupname").val().trim(),
-    restaurant_name: name,
-    address: address,
-    cuisines: cuisines,
-    phone: phone,
-    rating: rating,
-    photo: image,
-    website: website,
-  };
+  var restaurant = $("#restaurant-name").val().trim();
 
-  $.post("api/restaurants", restaurant, function(data) {
-    console.log("data saved", data);
-    $("#username").val("");
-    $("#new-restaurant").val("");
-  });
-  
-} 
+  //$("#group-form input").val("");
+    var settings = yelpSearchSettings(restaurant);
+
+    $.ajax(settings).done(function (response) {
+
+      var data = response.businesses[0];
+        parseYelp(data);
+        console.log(data);
+    })
+};
+
+function parseYelp(data) {
+    var name = data.name;
+    var address = data.location.display_address[0];
+    var phone = data.display_phone;
+    var rating = data.rating;
+    var photo = data.image_url;
+    var website = data.url;
+    console.log(data);
+    restaurantDB(name, address, phone, rating, photo, website);
+};
+
+
+function restaurantDB(name, address, phone, rating, photo, website) {
+    var restaurant = {
+        user_name: $("#username").val().trim(),
+        group_name: $("#groupName").text().trim(),
+        restaurant_name: name,
+        address: address,
+        phone: phone,
+        rating: rating,
+        photo: photo,
+        website: website,
+    }
+    $.post("/api/newRestaurant", restaurant)
+
+        .then(function (data) {
+            console.log("yelp api data saved to db: ", data);
+            $("#username").val(""); 
+            $("#location").val(""); 
+            $("#restaurant-name").val("");
+            // $("#submit-pick").removeClass("ld ld-over-full-inverse running");
+        })
+};
